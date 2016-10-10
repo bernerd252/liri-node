76674213dca39074515c14bff8ca9a79d70ba499
Eedouard Batlle <Eedouard Batlle>
2016-10-10 16:13:21 -0400

    10/10/16 16:13

--- liri.js
+++ liri.js
@@ -33,6 +33,20 @@ request(queryUrl, function (error, response, body) {
 		console.log(JSON.parse(body)["Year"]);
 		console.log("Plot: " + JSON.parse(body)["Plot"]);
 		console.log("Starring: " + JSON.parse(body)["Actors"]);
+		 var fs = require('fs'); 
+
+		// This block of code will create a file called "movies.txt".
+		// It will then print "Inception, Die Hard" in the file
+		fs.appendFile("movies.txt", movieName + "  ", function(err) {
+		    
+			// If the code experiences any errors it will log the error to the console. 
+		    if(err) {
+		        return console.log(err);
+		    }
+
+		    // Otherwise, it will print: "movies.txt was updated!"
+		    console.log("movies.txt was updated!");
+		}); 
 
 	}
 });
@@ -40,31 +54,113 @@ request(queryUrl, function (error, response, body) {
 
 if (process.argv[2] == "spotify"){
 	var spotify = require('spotify');
- 
-spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+ 	var movieName = []
+// Then run a request to the OMDB API with the movie specified 
+	for (x=3;x<process.argv.length;x++)
+	{	var movieWord = process.argv[x]
+		movieName.push(movieWord)
+	}
+spotify.search({ type: 'track', query: movieName }, function(err, data) {
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
     }
  	
-    // Do something with 'data'
-    console.log(data)
+    console.log("That is a song by " + data.tracks.items[0].artists[0].name)
+    console.log("called " + data.tracks.items[0].name)
+    console.log("You can find a preview here " + data.tracks.items[0].preview_url)
+    console.log("And it's on the album " + data.tracks.items[0].album.name)
+
+    var fs = require('fs'); 
+
+// This block of code will create a file called "movies.txt".
+// It will then print "Inception, Die Hard" in the file
+fs.appendFile("spotify.txt", movieName + "  ", function(err) {
+    
+	// If the code experiences any errors it will log the error to the console. 
+    if(err) {
+        return console.log(err);
+    }
+
+    // Otherwise, it will print: "movies.txt was updated!"
+    console.log("spotify.txt was updated!");
+}); 
 });
 }
 
-// if (process.argv[2] == "twitter"){
-// 	var Twitter = require('twitter');
-//  	var keys = require("./keys.js")
-// 	var client = new Twitter({
-// 	  consumer_key: keys.consumer_key,
-// 	  consumer_secret: keys.consumer_secret,
-// 	  access_token_key: keys.access_token_key,
-// 	  access_token_secret: keys.access_token_secret
-// 	});
-// 	console.log(client)
-// 	var params = {screen_name: 'nodejs'};
-// 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-// 	  if (!error) {
-// 	    console.log(tweets);
-// 	  }
-// 	});
+if (process.argv[2] == "twitter"){
+var Twitter = require('twitter');
+var keys = require("./keys.js") 
+var client = new Twitter({
+  consumer_key: "sdCowAT8TUYdLdxArVsQrn13X",
+  consumer_secret: "G8NjOwQL8QjcZnrHRpOU9ctTHijTwyLhgozBaMU3V7tnkGjal2",
+  access_token_key: "784178709167308801-tXLroj2pvzwrl4dESMzAOrblH7aoKdx",
+  access_token_secret: "RZAOM4G0zeGOsQLbPhuQBOfus0ycnndBDBV8RyeIqVelc"
+});
+ // console.log (keys)
+var screen_name = process.argv[3];
+var params = {screen_name: screen_name};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+  	for (x=0;x<20;x++)
+    console.log(tweets[x].text);
+	  var fs = require('fs'); 
+
+	// This block of code will create a file called "movies.txt".
+	// It will then print "Inception, Die Hard" in the file
+	fs.appendFile("twitter.txt", screen_name + "  ", function(err) {
+	    
+		// If the code experiences any errors it will log the error to the console. 
+	    if(err) {
+	        return console.log(err);
+	    }
+
+	    // Otherwise, it will print: "movies.txt was updated!"
+	    console.log("twitter.txt was updated!");
+	}); 
+  }
+});
+}
+
+if (process.argv[2] == "do-what-it-says"){
+	var fs = require('fs');
+	var spotify = require('spotify');
+	fs.readFile("random.txt", "utf8", function(error, data){
+		console.log(data);
+
+	var dataArr = data.split(' ');
+
+	// console.log(dataArr);
+	var movieName = []
+	for (x=1;x<dataArr.length;x++)
+	{	var movieWord = dataArr[x]
+		movieName.push(movieWord)
+	}
+	// console.log(movieName)
+	spotify.search({ type: 'track', query: movieName }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 	
+    console.log("That is a song by " + data.tracks.items[0].artists[0].name)
+    console.log("called " + data.tracks.items[0].name)
+    console.log("You can find a preview here " + data.tracks.items[0].preview_url)
+    console.log("And it's on the album " + data.tracks.items[0].album.name)
+
+
+// This block of code will create a file called "movies.txt".
+// It will then print "Inception, Die Hard" in the file
+fs.appendFile("do-what-it-says.txt", movieName + "  ", function(err) {
+    
+	// If the code experiences any errors it will log the error to the console. 
+    if(err) {
+        return console.log(err);
+    }
+
+    // Otherwise, it will print: "movies.txt was updated!"
+    console.log("do-what-it-says.txt was updated!");
+}); 
+});
+});
+}
